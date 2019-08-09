ddab641dc8ba1e533974db0ddba49af49771e26f
Harit Kumar <haritkumar@hotmail.com>
2019-08-09 17:16:43 +0530

    base

---
+++ src/main/java/com/si/DbPollerSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPollerSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPollerSpringIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/si/DbPollerSpringIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.si;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbPollerSpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
